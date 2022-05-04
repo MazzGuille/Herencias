a6953be345465d15846aa7a6abbec12a3b2a9cd2
MazzGuille <mazzcode@gmail.com>
2022-05-04 01:34:06 -0300

    tuples

---
+++ TestLibrary/tuplas.cs
@@ -0,0 +1,7 @@
+namespace TestLibrary
+{
+    public class tuplas
+    {
+         (string, double, int) GuillermoMazzari = ("Guille", 1.69d, 35);
+    }
+}
--- TestUI/Program.cs
+++ TestUI/Program.cs
@@ -13,3 +13,11 @@ Console.WriteLine((int)Estaciones.Primavera);
 Console.WriteLine((int)Estaciones.Verano);
 Console.WriteLine((int)Estaciones.Otoño);
 Console.WriteLine((int)Estaciones.Invierno);
+
+
+//*Tuplas
+//*Tuplas son una forma de almacenar una colección de datos.
+Console.WriteLine(tuplas.GuillermoMazzari.Item1);
+Console.WriteLine(tuplas.GuillermoMazzari.Item2);
+Console.WriteLine(tuplas.GuillermoMazzari.Item3);
+Console.WriteLine(tuplas.GuillermoMazzari);
